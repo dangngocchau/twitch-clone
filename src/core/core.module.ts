8ce2098c09c1f5e3179a5f9750b6e2cd8d42971f
Chau Dang <dnchau2310@gmail.com>
2025-02-20 01:19:45 +0700

    Set up graphQl and Redis

---
+++ src/core/core.module.ts
@@ -0,0 +1,30 @@
+import { ApolloDriver } from '@nestjs/apollo'
+import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql'
+
+import { getGraphQLConfig } from '@/src/core/config/graphql.config'
+import { PrismaModule } from '@/src/core/prisma/prisma.module'
+import { AccountModule } from '@/src/modules/auth/account/account.module'
+import { IS_DEV_ENV } from '@/src/shared/utils/is-dev.util'
+
+import { RedisModule } from './redis/redis.module'
+
+@Module({
+	imports: [
+		ConfigModule.forRoot({
+			ignoreEnvFile: !IS_DEV_ENV,
+			isGlobal: true
+		}),
+		GraphQLModule.forRootAsync({
+			driver: ApolloDriver,
+			imports: [ConfigModule],
+			useFactory: getGraphQLConfig,
+			inject: [ConfigService]
+		}),
+		PrismaModule,
+		RedisModule,
+		AccountModule
+	]
+})
+export class CoreModule {}
